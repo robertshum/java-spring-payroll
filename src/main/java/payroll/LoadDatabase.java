408b7640c92dbc79210e0c28e37bd7720038228d
robertshum <robertshum@gmail.com>
2024-02-06 22:04:08 -0800

    add order controller and support classes

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -12,12 +12,23 @@ public class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Robert", "Shum", "Software Developer")));
+      // Employees
+      log.info("Preloading " + employeeRepository.save(new Employee("Robert", "Shum", "Software Developer")));
 
-      log.info("Preloading " + repository.save(new Employee("George", "Washington", "President")));
+      log.info("Preloading " + employeeRepository.save(new Employee("George", "Washington", "President")));
+
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+      // Orders
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      });
     };
   }
 }
